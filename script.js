07724be35785c3c651d0204300d6443275db018d
elfercape <elfercape@gmail.com>
2026-02-03 21:45:16 -0300

    agregando script.js 03-02-2026

---
+++ script.js
@@ -0,0 +1,21 @@
+const nombres = ['ana', 'carlos', 'elena', 'fernando', 'joseline'];
+console.log('Arreglo inicial:');
+console.log(nombres);
+console.log('-------------------');
+
+//for (inicio;condicion;incremento)
+console.log('largo de Arreglo:', nombres.length);
+for (i = 0; i < nombres.length; i++) {
+  console.log('index:', i);
+  console.log('nombre:', nombres[i]);
+  console.log('----------------------');
+}
+// forEach es un metodo de JS
+// forEach(arg...)
+// forEach(()=>{}) o forEach(()=>)
+// function nombredefunction(props...) --> (props)=>{}
+// (props* cuando son mas de una se pasan entre comas)
+nombres.forEach((n, i) => {
+  console.log('indice desde ForEach:', i);
+  console.log('nombre desde ForEach:', n);
+});
